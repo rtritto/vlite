bdbad052e252cec3c51c6cad766058df0893235b
rtritto <ariele.1994@gmail.com>
2025-02-22 23:48:09 +0100

    Move setSource method from player core to html5 provider

--- src/core/player.ts
+++ src/core/player.ts
@@ -478,15 +478,6 @@ export default class Player {
 	}
 
 	/**
-	 * Set the media source
-	 * @param source New media source URL
-	 */
-	setSource(source: string) {
-		this.media.src = source
-		this.media.load()
-	}
-
-	/**
 	 * Request the fullscreen
 	 */
 	requestFullscreen() {
